e9ba2d6255264de5bf5cd702afd03cb657b00601
overkilledit <luojames52@gmail.com>
2016-12-20 12:32:00 -0800

    swap location function and max
    New functions YAY

--- vectorFunction.h
+++ vectorFunction.h
@@ -35,3 +35,27 @@ int add (const vector <int>& UserVec){
     }
     return userTotal;
 }
+
+void swapLocation (vector <int>& UserVec){
+    int i=0;
+    int j=UserVec.size()-1;
+    while (i<j){
+        int temp = UserVec.at(i);
+        UserVec.at(i) = UserVec.at(j);
+        UserVec.at(j) = temp;
+        i++;
+        j--;
+    }
+}
+
+
+
+int max (const vector <int>& UserVec){
+    int maxNum = UserVec.at(0);
+    for (int i=1; i< UserVec.size(); i++){
+        if (UserVec.at(i) > maxNum){
+            maxNum = UserVec.at(i);
+        }
+        }
+        return maxNum;
+}
